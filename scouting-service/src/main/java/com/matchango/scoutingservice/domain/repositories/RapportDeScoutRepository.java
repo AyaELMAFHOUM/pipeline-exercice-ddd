0574cfa9870131378143317421f458cc66c8bbf6
Houssam eddine Haddouche <hhaddouc@c3r8p3.1337.ma>
2025-04-18 11:22:35 +0100

    Keep folder structure with .gitkeep files

--- scouting-service/src/main/java/com/matchango/scoutingservice/domain/repositories/RapportDeScoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matchango.scoutingservice.domain.repositories;
-
-import com.matchango.scoutingservice.domain.model.RapportDeScout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RapportDeScoutRepository extends JpaRepository<RapportDeScout, UUID> {
-}
