c1826ee145321979d5d06f5836825ec3fd63b331
Gowthertg <houssamhaddouche777@gmail.com>
2025-04-23 22:21:25 +0100

    TEST: add test for age filter

--- scouting-service/src/main/java/com/matchango/scoutingservice/infrastructure/web/dto/JoueurWithNoteDto.java
+++ scouting-service/src/main/java/com/matchango/scoutingservice/infrastructure/web/dto/JoueurWithNoteDto.java
@@ -1,9 +1,13 @@
 package com.matchango.scoutingservice.infrastructure.web.dto;
 import com.matchango.scoutingservice.domain.model.Position;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class JoueurWithNoteDto {
     private String nom;
